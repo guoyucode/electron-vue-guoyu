3a781ffae22bfef08a17ac7f1f9cda5618fcb498
guoyu <guoyumail@qq.com>
2019-09-19 20:59:31 +0800

    sqlite3数据库访问依赖

--- electron-main/main.js
+++ electron-main/main.js
@@ -8,7 +8,7 @@ let updateHandle = require("./update")
 let {showTray, checkForUpdates} = require("./tray")
 let setHotKeyFun = require("./hotKey")
 
-//let v = require("./sqlite/sqlite_util")
+let v = require("./sqlite/sqlite_util")
 
 let isDev = process.env.NODE_ENV && process.env.NODE_ENV == 'development'
 let env_openChromeDevTools = !!process.env.openChromeDevTools;
